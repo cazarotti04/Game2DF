f9bb7819cf39796fb0777d025dedd69de9ff9433
cazarotti04 <cazarotti04@gmail.com>
2017-12-06 02:06:41 -0200

    Final sem os monstros

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour {
+
+	public static GM instance = null;
+
+	public float yMinLive = -10f;
+	public Transform spawnPoint;
+	public GameObject playerPrefab;
+
+	public float maxTime = 120f;
+
+	bool timerOn = true;
+	float timeLeft;
+
+
+	public UI ui;
+
+	GameData data = new GameData(); 
+
+	PlayerController player;
+
+	public float timeToResp = 2f;
+
+	void Awake(){
+		if(instance == null){
+			instance = this;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		if(player == null){
+			RespawnPlayer();
+		}
+		timeLeft = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player==null){
+			GameObject obj = GameObject.FindGameObjectWithTag("Player");
+			if(obj != null){
+				player = obj.GetComponent<PlayerController>();
+			}
+		}
+		UpdateTimer(); 
+		DisplayHudData();
+	}
+
+	public void restartLevel(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void ExitToMainMenu(){
+		LoadScene("MainMenu");
+	}
+
+	public void closeApp(){
+		Application.Quit();
+	}
+
+	public void LoadScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+
+	void UpdateTimer(){
+		if(timerOn){
+			timeLeft = timeLeft - Time.deltaTime;
+			if(timeLeft <= 0){
+				timeLeft = 0;
+				ExpirePlayer();
+			}
+		}
+	}
+
+	void DisplayHudData(){
+		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtLifeCount.text = "x " + data.lifeCount;
+		ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
+	}
+
+	public void IncrementCoinCount(){
+		data.coinCount++;
+	}
+
+	public void RespawnPlayer(){
+		Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+	}
+
+	public void killPlayer(){
+		if(player!=null){
+			Destroy(player.gameObject);
+			DecrementLife();
+			if(data.lifeCount>0){
+				Invoke("RespawnPlayer", timeToResp);
+			}
+			else{
+				GameOver();
+			}
+		}
+	}
+
+	public void ExpirePlayer(){
+		if(player!=null){
+			Destroy(player.gameObject);
+		}
+		GameOver();
+	}
+
+	void GameOver(){
+		timerOn = false;
+		ui.gameOver.txtCoinCount.text = "Coins - " + data.coinCount;
+		ui.gameOver.txtTimer.text = "Timer - " + timeLeft.ToString("F1");
+		ui.gameOver.gameOverPanel.SetActive(true);
+	}
+
+	public void DecrementLife(){
+		data.lifeCount--;
+	}
+
+	public void LevelComplete(){
+		Destroy(player.gameObject);
+		timerOn = false;
+		ui.levelComplete.txtCoinCount.text = "Coins - " + data.coinCount;
+		ui.levelComplete.txtTimer.text = "Timer - " + timeLeft.ToString("F1");
+		ui.levelComplete.LevelCompletePanel.SetActive(true);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+0 - idle
+1 - run
+2 - jump
+3 - fall
+4 - land
+5 - attack
+6 - dead
+ */
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float horizontalSpeed = 10f;
+	public float jumpSpeed = 600f;
+
+	Rigidbody2D rb;
+
+	SpriteRenderer sr;
+
+	Animator anao;
+
+	bool isJumping = false;
+
+	public Transform Feet;
+	public float feetW = 0.5f;
+	public float feetH = 0.1f;
+
+	public bool isGrounded;
+	public LayerMask whatIsGround;
+
+	bool canDoubleJump = false;
+	public float delayForDJ = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		sr = GetComponent<SpriteRenderer>();
+		anao = GetComponent<Animator>();
+	}
+	
+	void OnDrawGizmos(){
+			Gizmos.DrawWireCube(Feet.position, new Vector3(feetW, feetH, 0f));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if(transform.position.y < GM.instance.yMinLive){
+			GM.instance.killPlayer();
+		}
+
+		isGrounded = Physics2D.OverlapBox(new Vector2(Feet.position.x, Feet.position.y), new Vector2(feetW, feetH), 360.0f, whatIsGround);
+
+		float horizontalInput = Input.GetAxisRaw("Horizontal");//-1 = Left // 1 = Right
+		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+
+		if(horizontalPlayerSpeed != 0){
+			MoveHorizontal(horizontalPlayerSpeed);
+		}
+		else{
+			StopMovingHorizontal();
+		}
+
+		if(Input.GetButtonDown("Jump")){
+			Jump();
+		}
+		
+		ShowFall();
+	}
+
+	void MoveHorizontal(float speed){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+
+		if(speed < 0f){
+			sr.flipX = true;
+		}
+		else if(speed > 0f){
+			sr.flipX = false;
+		}
+		if(!isJumping){
+			anao.SetInteger("State", 1);
+		}
+	}
+
+	void StopMovingHorizontal(){
+		rb.velocity = new Vector2(0f,rb.velocity.y);
+		if(!isJumping){
+			anao.SetInteger("State", 0);
+		}
+	}
+
+	void ShowFall(){
+		if(rb.velocity.y<0f){
+			anao.SetInteger("State", 3);
+		}
+	}
+
+	void Jump(){
+		if(isGrounded){
+			isJumping = true;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+			AudioManager.instance.PlayJumpSound(gameObject);
+			anao.SetInteger("State", 2);
+
+			Invoke("EnableDJ", delayForDJ);
+		}
+
+		if(canDoubleJump && !isGrounded){
+			rb.velocity = Vector2.zero;
+			rb.AddForce(new Vector2(0f, jumpSpeed));
+			AudioManager.instance.PlayJumpSound(gameObject);
+			anao.SetInteger("State", 2);
+			canDoubleJump = false;	
+		}
+	}
+
+	void EnableDJ(){
+		canDoubleJump = true;
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.layer == LayerMask.NameToLayer("Ground")){
+			isJumping = false;
+		}
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+		switch(other.gameObject.tag){
+			case "Coin":
+				AudioManager.instance.PlayCoinPickupSound(other.gameObject);
+           		SFXManager.instance.ShowCoinParticles(other.gameObject);
+				GM.instance.IncrementCoinCount();
+            	Destroy(other.gameObject);
+				break;
+			
+			case "Finish":
+				GM.instance.LevelComplete();
+
+				break;
+		}
+
+        if (other.gameObject.CompareTag("Coin"))
+        {
+			
+        }
+    }
+
+}
